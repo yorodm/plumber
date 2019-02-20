d5038c80c188fa41952c58fbb15081237c028c5b
Yoandy Rodriguez Martinez <yoandy.rmartinez@gmail.com>
2019-02-20 15:07:44 -0500

    Travis stuff

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 with open("README.md") as rd:
     long_description = rd.read()
 
-VERSION = '0.1dev'
+VERSION = '0.1.0'
 
 setup(
     name='plumber',
+    author='Yoandy Rodriguez Martinez',
+    author_email='yoandy.rmartinez@gmail.com',
+    license='LICENSE',
     version=VERSION,
     packages=['plumber', ],
-    license='MIT',
     long_description=long_description
 )
