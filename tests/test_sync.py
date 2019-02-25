b86912e0b69ee6c7db8ab3615394c10f664fcd6c
Yoandy Rodriguez Martinez <yoandy.rmartinez@gmail.com>
2019-02-25 11:12:43 -0500

    Adding new tests

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -8,23 +8,45 @@ def test_pipe(extractor, transformer, writer):
     assert len(writer._data) == 10
 
 
-def test_from_function(extractor, writer):
+def test_iterable_extractor(transformer, writer):
+    data = plumber.extractor(range(2))
+    pipe = plumber.Pipe(data, transformer, writer)
+    pipe.run()
+    assert writer._data == [0, 1, 3]
+
+
+def test_extractor_decorator(transformer, writer):
+    @plumber.extractor
+    def data():
+        for x in range(10):
+            yield {'a':x}
+    pipe = plumber.Pipe(data, transformer, writer)
+    pipe.run()
+    assert writer._data[0] == {'a': 0}
+
+def test_combine_extractors(extractor, transformer, writer):
+    data = extractor(range(2))
+    pipe = plumber.Pipe(extractor + data, transformer, writer)
+    pipe.run()
+    assert writer._data[0] == ({"a": 0}, 0)
+    assert writer._data[3] == ({"a": 3}, None)
+
+
+def test_transformer_decorator(extractor, writer):
+    @plumber.transformer
     def dupe(a):
         return (a, a)
 
-    pipe = plumber.Pipe(
-        extractor, plumber.Transformer.from_function(dupe), writer
-    )
+    pipe = plumber.Pipe(extractor, dupe, writer)
     pipe.run()
     assert writer._data[0] == ({"a": 0}, {"a": 0})
 
 
 def test_chaining(extractor, transformer, writer):
+    @plumber.transformer
     def dupe(a):
         return (a, a)
-    dupe_transformer = plumber.Transformer.from_function(dupe)
-    pipe = plumber.Pipe(
-        extractor, transformer + dupe_transformer + transformer, writer
-    )
+
+    pipe = plumber.Pipe(extractor, transformer + dupe + transformer, writer)
     pipe.run()
     assert writer._data[0] == ({"a": 0}, {"a": 0})
