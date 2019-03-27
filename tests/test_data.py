49ad47d1c6fb6aa7a604c7c88bea82775dc8f830
Yoandy Rodriguez Martinez <yoandy.rmartinez@gmail.com>
2019-03-27 15:38:15 -0400

    Test for CsvExtractor

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,13 @@
 # coding: utf-8
-import plumber
+import plumber.extractors
+from pathlib import Path
 
 
-def test_csv(writer):
-    pass
+def test_csv(writer, transformer):
+    current_dir = Path(__file__).resolve().parent
+    filename = current_dir.joinpath('test_data.csv')
+    extractor = plumber.extractors.CsvExtractor(filename)
+    pipe = plumber.pipe.Pipe(extractor, transformer, writer)
+    pipe.run()
+    print(type(writer._data[0]))
+    assert writer._data[0] == ['Github', 'Super git repos', '1']
